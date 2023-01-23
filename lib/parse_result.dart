fd16ea57fdee373024ca4857292e48bc0df1d3a2
BhavyaTripathi <tripathii.bhavya@gmail.com>
2023-01-23 12:08:51 +0530

    new commit

--- lib/parse_result.dart
+++ lib/parse_result.dart
@@ -1,10 +1,10 @@
 import 'package:noble_winners/json_data.dart';
 
 class ParseResult{
-  Map<String,String> m1 ={};
-  Map<String,String> m2 ={};
-  Map<String,String> m3 ={};
-  Map<String,String> m4 ={};
+  Map<String,dynamic> m1 ={};
+  Map<String,dynamic> m2 ={};
+  Map<String,dynamic> m3 ={};
+  Map<String,dynamic> m4 ={};
   void getData(){
     DataWinners().awards.forEach((element) {
       if(element['category']=='Physics'){
@@ -28,21 +28,42 @@ class ParseResult{
         }
       }
     });
-    print(m1);
-    print("*********");
-    print(m2);
-    print("*********");
-    print(m3);
-    print("*********");
-    print(m4);
-    print(getCount(m1));
-    print(getCount(m2));
-    print(getCount(m3));
-    print(getCount(m4));
+    // print(m1);
+    // print("*********");
+    // print(m2);
+    // print("*********");
+    // print(m3);
+    // print("*********");
+    // print(m4);
+    // print(getCount(m1));
+    // print(getCount(m2));
+    // print(getCount(m3));
+    // print(getCount(m4));
+
+    getRes(m1, getShare(getCount(m1)));
+    getRes(m2, getShare(getCount(m2)));
+    getRes(m3, getShare(getCount(m3)));
+    getRes(m4, getShare(getCount(m4)));
 
   }
-  Map getCount(Map m1){
-    Map<String,int> count ={};
+  getRes(Map<String,dynamic> m1, Map<String,double> c1){
+    m1.forEach((key, value) {
+      if(c1.containsKey(value)){
+        m1.update(key, (value1) => c1[value]);
+      }
+    });
+    print(m1);
+  }
+  getShare(Map<String,double> c1){
+    int l =c1.length;
+    double eachContri = 1/l;
+    c1.forEach((key, value) {
+      c1.update(key, (value) => eachContri/value);
+    });
+    return c1;
+  }
+  Map<String, double> getCount(Map m1){
+    Map<String,double> count ={};
     m1.forEach((key, value) {
       if(!count.containsKey(value)){
         count.putIfAbsent(value, () => 1);
